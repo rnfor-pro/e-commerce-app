d30657a9ac46cd052d48c00d868316935045382b
rnfor-pro <67124388+rnfor-pro@users.noreply.github.com>
2024-08-09 09:52:58 -0400

    Update userdata2.sh

--- userdata2.sh
+++ userdata2.sh
@@ -6,9 +6,9 @@ sudo yum install httpd -y
 # Install Git
 sudo yum install git -y
 # Clone the repository
-git clone https://github.com/JJTechInc/ecommerce-web-app.git
+git clone https://github.com/rnfor-pro/e-commerce-app.git
 # Copy the files inside the cloned folder to the desired location
-sudo cp -r ecommerce-web-app/server1/* /var/www/html/
+sudo cp -r e-commerce-app/server1/* /var/www/html/
 # To get the current hostname address
 echo "<center><p>Running the website from instance host: $(hostname -f)</p></center>" >> /var/www/html/index.html
 # Start and enable the HTTP server
